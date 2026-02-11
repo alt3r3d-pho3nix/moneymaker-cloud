8d7088b0cbcb9212889b440ff84d61493d4c7569
alt3r3d-pho3nix <altrdphnx@gmail.com>
2026-02-11 10:49:41 -0800

    Finalizing: Real Wix Secret added

---
+++ moneymaker-cloud/src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'a0b7dee4-c625-4b60-8778-b7a840c2077a',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
